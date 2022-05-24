2a835e909188af51e28ae2665da42bdc42c1a0fa
sachinthaakesh <akeshramasinghe@gmail.com>
2022-05-24 13:53:49 +0530

    Update album.java
    develop a method to add song to album

--- src/song_player/album.java
+++ src/song_player/album.java
@@ -5,12 +5,34 @@
  */
 package song_player;
 
-
+import java.util.ArrayList;
 
 /**
  *
  * @author Akesh
  */
 public class album {
-
+    private String title;
+    private double duration;
+    private ArrayList<song> songs; //list of an array from the class 'song'
+    
+    public album(String title, double duration, ArrayList<song> songs){
+        this.title =  title;
+        this.duration = duration;
+        this.songs = new ArrayList<song>();
+    }
+    
+    public album(){}
+    
+    public boolean addSong(String title, double duration){
+    if(findSong(title) == null){
+        songs.add(new song(title,duration));
+        System.out.println("Successfully add song to album");
+        return true;
+    }
+    else{
+        System.out.println("Song already exsits in the album");
+        return false;
+    }
+    }
 }
